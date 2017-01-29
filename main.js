a66791ec8ec550b09256a72061f73bfd0e5e2bfa
Garett Arrowood <garettarrowood@gmail.com>
2017-01-29 09:39:58 -0500

    Add devDependencies and icon

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
-
+let path = require('path')
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width: 800, height: 600})
+  let win = new BrowserWindow({width: 800, height: 600, icon: path.join(__dirname, 'img/logo.icns')})
   win.loadURL(`file://${__dirname}/index.html`)
   // win.webContents.openDevTools()
 })
